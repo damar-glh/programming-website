6d265fcf3d55bc59853020d31584aa4a32f4a2f2
damara <damarmuhamad13@gmail.com>
2024-06-06 08:18:33 +0700

    feat: add Week_10(CRUD) in this project

---
+++ Week_10/connection.php
@@ -0,0 +1,14 @@
+<?php
+//Koneksi ke database
+$server    = "localhost";
+$usernames = "root";
+$passwords = "";
+$database  = "toko_baju"; //nama database
+$mysqli    = mysqli_connect($server, $usernames, $passwords, $database);
+//Check error, jikalau error tutup koneksi dengan mysql
+if (mysqli_connect_errno()) {
+    echo 'Koneksi gagal, ada masalah pada: ' . mysqli_connect_error();
+    exit();
+    mysqli_close($mysqli);
+}
+// mysqli_close($mysqli)
